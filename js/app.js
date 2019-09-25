7967fd36264bef353e42351d6893c04ee6a98214
Sylwia Fait <sylwia.fait@gmail.com>
2019-09-25 18:36:29 +0200

    Add members

--- js/app.js
+++ js/app.js
@@ -29,9 +29,31 @@ function changeTitle() {
     })
 }
 
+function readMore() {
+     const readMoreLessBtn = document.querySelectorAll(".read-more-less-btn");
+
+     function showHideText() {
+        const siblingText = this.previousElementSibling;
+
+       if (siblingText.style.display === "none" || siblingText.style.display === "") {
+            siblingText.style.display = "block";
+            this.textContent = "Czytaj mniej";
+        } else {
+            siblingText.style.display = "none";
+            this.textContent = "Czytaj więcej";
+        }
+    }
+
+    for (let i = 0; i < readMoreLessBtn.length; i++) {
+        readMoreLessBtn[i].addEventListener("click", showHideText);
+    }
+
+}
+
 const init = () => {
     showHamburgerMenu();
     changeTitle();
+    readMore();
 }
 
 document.addEventListener("DOMContentLoaded", init);
